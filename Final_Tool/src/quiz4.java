21f0acc2c9cd3848d7d896ee354d895983d40462
song2 <msms9957@gmail.com>
2020-06-27 07:31:29 +0900

    2020-06-27

---
+++ Final_Tool/src/quiz4.java
@@ -0,0 +1,13 @@
+
+public class quiz4 
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		quiz4_Frame frame = new quiz4_Frame(); //quiz4번의 frame 호출
+	}
+
+}
